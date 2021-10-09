b6fed9bff37b98ac19ab7bc104f2471b4f5b93ed
Sakib Noman <skbnmn@gmail.com>
2021-10-09 21:56:18 +0600

    init

---
+++ main.js
@@ -0,0 +1,31 @@
+const mainProducts = ['Apple', 'Google', 'Amazon', 'Facebook', 'WhatsApp']
+const app = Vue.createApp({
+    data() {
+        return {
+            products: mainProducts,
+            filterValue: '',
+            json: null
+
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.filterValue.length > 0) {
+                const filteredProduct = mainProducts.filter(each => each.toLowerCase().includes(this.filterValue.toLowerCase()))
+                this.products = filteredProduct;
+            }
+            else {
+                this.products = mainProducts;
+            }
+        }
+    },
+    created: function () {
+        fetch("./android.json")
+            .then(r => r.json())
+            .then(json => {
+                this.json = json;
+                this.products = json
+            })
+    }
+
+}).mount('#app')
