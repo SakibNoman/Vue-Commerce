4a60245bc80acfbf1ef7836873a883b72b7717f8
Sakib Noman <skbnmn@gmail.com>
2021-10-10 12:01:48 +0600

    filter msg

--- main.js
+++ main.js
@@ -3,32 +3,45 @@ const app = Vue.createApp({
         return {
             products: null,
             filterValue: '',
-            json: null
-
+            json: null,
+            showFilterMsg: false,
+            searchValue: '',
         }
     },
     methods: {
         onSubmit() {
-            if (this.filterValue.length > 0) {
-                const filteredProduct = this.json.filter(each => each.name.toLowerCase().includes(this.filterValue.toLowerCase()))
+            if (this.searchValue.length > 0) {
+                const filteredProduct = this.json.filter(each => each.name.toLowerCase().includes(this.searchValue.toLowerCase()))
+                this.filterValue = this.searchValue;
                 this.products = filteredProduct;
+                this.showFilterMsg = true;
             }
             else {
                 this.products = this.json;
+                this.showFilterMsg = false
             }
         },
         filterWithRating(star) {
             const filteredProduct = this.json.filter(each => each.star >= star)
             this.products = filteredProduct;
+            this.filterValue = star + ' star rating and up';
+            this.showFilterMsg = true;
         },
         filterWithPrice(minPrice, maxPrice) {
             const filteredProduct = this.json.filter(each => each.price >= minPrice && each.price <= maxPrice)
             this.products = filteredProduct;
+            this.filterValue = 'price between $' + minPrice + ' and $' + maxPrice;
+            this.showFilterMsg = true;
+        },
+        clearFilter() {
+            this.products = this.json;
+            this.showFilterMsg = false;
+            this.searchValue = ''
         }
     },
     created: function () {
         fetch("./android.json")
-            .then(r => r.json())
+            .then(res => res.json())
             .then(json => {
                 this.json = json;
                 this.products = json
